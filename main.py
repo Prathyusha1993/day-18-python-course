e1139ef161eb92012b0e548b04f42819f86e3773
Prathyusha Kurigala <prathyushakiran@Prathyushas-MBP.attlocal.net>
2025-01-13 12:38:19 -0800

    day 18 python challenge about turtle graphics

---
+++ main.py
@@ -0,0 +1,31 @@
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+timmy_the_turtle.shape('turtle')
+timmy_the_turtle.color('firebrick')
+
+# timmy_the_turtle.forward(100)
+# timmy_the_turtle.right(90)
+# timmy_the_turtle.forward(100)
+# timmy_the_turtle.right(90)
+# timmy_the_turtle.forward(100)
+# timmy_the_turtle.right(90)
+# timmy_the_turtle.forward(100)
+
+# using for loop
+for _ in range(4):
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.right(90)
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
+
